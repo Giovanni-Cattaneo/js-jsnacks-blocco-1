bb075cf22e1d2fb7f88819a861ff89af34668f4f
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-23 12:14:50 +0100

    format

--- snack-1/main.js
+++ snack-1/main.js
@@ -5,8 +5,8 @@ const secondNumber = prompt("Inserisci un nuovo numero")
 
 console.log(firstNumber, secondNumber);
 
-if(Number(firstNumber) > Number(secondNumber)){
+if (Number(firstNumber) > Number(secondNumber)) {
     console.log(firstNumber);
-}else(Number(firstNumber) < Number(secondNumber));{
- console.log(secondNumber);
+} else (Number(firstNumber) < Number(secondNumber)); {
+    console.log(secondNumber);
 }
