0190f5d760a14a5d4a10479c70d98ed2c293304b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-23 12:36:28 +0100

    complete

---
+++ snack-2/main.js
@@ -0,0 +1,14 @@
+console.log("hello world");
+
+const firstWord = prompt("Scrivi una parola")
+const secondWord = prompt("Scrivi un'altra parola")
+
+console.log(firstWord, secondWord);
+
+if(firstWord.length > secondWord.length){
+    console.log(firstWord);
+    console.log(secondWord);
+}else{
+    console.log(secondWord);
+    console.log(firstWord);
+}
