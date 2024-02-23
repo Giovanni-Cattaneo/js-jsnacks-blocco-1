13153ce936a8a78de24468bceeb96fdc8955267d
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-23 12:55:21 +0100

    completed

---
+++ snack-3/main.js
@@ -0,0 +1,23 @@
+console.log("hello world");
+
+let inviteList = ["Mario", "giorgio", "paolo", "fabrizio", "martino", "francesco", "alessia", "francesca"]
+
+console.log(inviteList);
+
+let guest = prompt("Inserisci il tuo nome")
+
+console.log(guest);
+
+let invited = false;
+
+for (let i = 0; i < inviteList.length; i++) {
+    const element = inviteList[i];
+
+    if (guest === element) {
+        invited = true
+    }
+}
+
+console.log(invited);
+
+
