a12a2fcf4abc818747bb8bbfb1bc407c65e43ce2
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-23 12:14:29 +0100

    completed

---
+++ snack-1/main.js
@@ -0,0 +1,12 @@
+console.log("hello world");
+
+const firstNumber = prompt("Inserisci un numero")
+const secondNumber = prompt("Inserisci un nuovo numero")
+
+console.log(firstNumber, secondNumber);
+
+if(Number(firstNumber) > Number(secondNumber)){
+    console.log(firstNumber);
+}else(Number(firstNumber) < Number(secondNumber));{
+ console.log(secondNumber);
+}
---
+++ snack-1/readme.md
@@ -0,0 +1,4 @@
+Create una sotto-cartella per ogni jsnack e riportate la consegna all'inizio del file js. La struttura dei file dovrebbe essere la seguente: jsnack1 (cartella) - index.html - script.js jsnack2 (cartella) - index.html - script.js e così via
+Primo snack
+L’utente inserisce due numeri in successione, con due prompt.
+Il software stampa il maggiore.
