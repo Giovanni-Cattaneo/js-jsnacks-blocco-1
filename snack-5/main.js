35d500b3860f354280674b5aecfcef5c484cb0c7
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 12:37:57 +0100

    edit and completed

--- snack-5/main.js
+++ snack-5/main.js
@@ -2,18 +2,26 @@ console.log("hello world");
 
 
 
-let index = true;
+// let index = true;
 
-while ( index === true) {
-    let inputNumber = parseInt(prompt("inserisci un numero"))
+// while ( index === true) {
+//     let inputNumber = parseInt(prompt("inserisci un numero"))
     
-    console.log(inputNumber);
+//     console.log(inputNumber);
 
-    if (isNaN(inputNumber)) { // verificare se il carattere inserito restituisca un numero valido o meno
-        index = false
-    } else{ // se il numero del prompt è valido restituisci il cubo
-        let cube = inputNumber * inputNumber * inputNumber // cubo di input number
+//     if (isNaN(inputNumber)) { // verificare se il carattere inserito restituisca un numero valido o meno
+//         index = false
+//     } else{ // se il numero del prompt è valido restituisci il cubo
+//         let cube = inputNumber * inputNumber * inputNumber // cubo di input number
 
-        console.log(cube);
-    }
-} // il loop si chiude quando si preme cancel perchè index passa a false
+//         console.log(cube);
+//     }
+// } // il loop si chiude quando si preme cancel perchè index passa a false
+
+
+let inputNumber = parseInt(prompt("Scrivi un numero"))
+
+for (let index = 1; index < inputNumber; index++) {
+    console.log(index * index * index);
+    
+}
