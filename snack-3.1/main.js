1f23ec2e5ad8b016b20d8977be69e088e8195d29
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 11:01:42 +0100

    completed

--- snack-3.1/main.js
+++ snack-3.1/main.js
@@ -1,8 +1,12 @@
 console.log("hello World");
 
-let number = prompt("Scrivi un numero")
+let sum = 0
 
-for (let index = 0; index < number.length; index++) {
-    const element = number[index];
-    
+for (let index = 0; index < 10; index++) {
+    number = prompt("scrivi un nuovo numero")
+    console.log(number);
+
+    sum = sum + Number(number);
 }
+
+console.log(sum);
