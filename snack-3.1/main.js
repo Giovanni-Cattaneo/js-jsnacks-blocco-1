ad0031f59c499df2c0e1774b89fd1a897195e456
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 11:28:28 +0100

    add comment

--- snack-3.1/main.js
+++ snack-3.1/main.js
@@ -2,11 +2,25 @@ console.log("hello World");
 
 let sum = 0
 
-for (let index = 0; index < 10; index++) {
+for (let index = 0; index < 3; index++) {
     number = prompt("scrivi un nuovo numero")
     console.log(number);
 
     sum = sum + Number(number);
+}
+
+console.log(sum);
+
+
+let index = 0;
+
+while ( index < 3) {
+    number = prompt("scrivi un nuovo numero")
+    console.log(number);
+
+    sum = sum + Number(number); // usare sum += Number(number) sarebbe equivalente
+
+    index++
 }
 
 console.log(sum);
