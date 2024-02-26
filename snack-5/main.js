6adbe9ad68b8636a994e0077cd7c0ba91148d50e
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 12:30:12 +0100

    add comment

--- snack-5/main.js
+++ snack-5/main.js
@@ -9,11 +9,11 @@ while ( index === true) {
     
     console.log(inputNumber);
 
-    if (isNaN(inputNumber)) {
+    if (isNaN(inputNumber)) { // verificare se il carattere inserito restituisca un numero valido o meno
         index = false
-    } else{
-        let cube = inputNumber * inputNumber * inputNumber
+    } else{ // se il numero del prompt è valido restituisci il cubo
+        let cube = inputNumber * inputNumber * inputNumber // cubo di input number
 
         console.log(cube);
     }
-}
+} // il loop si chiude quando si preme cancel perchè index passa a false
