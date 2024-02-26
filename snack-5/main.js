151b3dc10c07a23a97afea5b84153acc9c6679b7
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 12:17:04 +0100

    add while

---
+++ snack-5/main.js
@@ -0,0 +1,17 @@
+console.log("hello world");
+
+
+
+let index = true;
+
+while ( index === true) {
+    let inputNumber = parseInt(prompt("inserisci un numero"))
+    
+    console.log(inputNumber);
+
+    let cube = inputNumber * inputNumber * inputNumber
+
+    console.log(cube);
+
+    index++
+}
