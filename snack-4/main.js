bcca91e871ecfbe1b82de466e1e0341531af7a3f
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-26 11:51:27 +0100

    add while

--- snack-4/main.js
+++ snack-4/main.js
@@ -14,3 +14,18 @@ for (let index = 0; index < 3; index++) {
 
 
 console.log(listNumber);
+
+let index = 0
+while (index < 3) {
+    let number = parseInt(prompt("Scrivi un numero"))
+    console.log(number);
+
+    if (number % 2 === 0) {
+    } else{
+        listNumber.push(number)
+    }
+
+    index++
+}
+
+console.log(listNumber);
