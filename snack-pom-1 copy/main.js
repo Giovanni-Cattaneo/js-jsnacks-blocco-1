d7ce8efecaedcb95b1ef9ec5db1c06b5a3039fe4
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-28 13:04:10 +0100

    edit

--- snack-pom-1 copy/main.js
+++ snack-pom-1 copy/main.js
@@ -20,11 +20,36 @@ console.log(box);
 
 
 
+
+
+const randomNumbers = []
+
+while ( randomNumbers.length <= 64) {
+    
+    const randomNumber = getRndInteger(1, maxxCellsNumber);
+
+    console.log(randomNumber);
+    
+    if (!randomNumbers.includes(randomNumber)) {
+        randomNumbers.push(randomNumber)
+    }
+    
+    randomNumbers.length++
+}
+
 for (let index = 0; index < box.length; index++) {
     const element = box[index];
     console.log(element);
     element.addEventListener("click", function(e){
         element.classList.toggle("red")
     })
+
 }
 
+console.log(randomNumbers);
+
+
+
+function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
