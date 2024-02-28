681e7b3217c1f8e9cb728b90cfa8bdd61129b62b
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-02-28 12:30:33 +0100

    add griglia

--- snack-pom-1 copy/main.js
+++ snack-pom-1 copy/main.js
@@ -1,1 +1,30 @@
 console.log("hello world");
+
+const containerEl = document.getElementById("container")
+
+console.log(containerEl);
+
+const maxxCellsNumber = 64;
+
+const markup = `<div class="box"></div>`
+
+for (let index = 0; index < maxxCellsNumber; index++) {
+    containerEl.insertAdjacentHTML("beforeend", markup)
+}
+
+
+
+const box = document.getElementsByClassName("box")
+
+console.log(box);
+
+
+
+for (let index = 0; index < box.length; index++) {
+    const element = box[index];
+    console.log(element);
+    element.addEventListener("click", function(e){
+        element.classList.toggle("red")
+    })
+}
+
